c32e20dd21a4a987f7d7a77f86c39541c1b30ddf
sms8179 <steven.shaughnessy1@gmail.com>
2024-01-09 19:10:38 -0600

    styling updates

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,10 @@ function App() {
 						<button id="button_connect" className='pill_button'>
 							Connect
 							<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 10.156 10.392">
-								<rect id="Rectangle_5" data-name="Rectangle 5" width="10.392" height="10.156" />
-								<path id="Path_2" data-name="Path 2" d="M10.392,1.855,8.616,0H0V8.348l1.965,1.808V6.021q0-1.509-.032-2.736l6.9,6.682L10.2,8.506,3.3,1.824q1.227.032,2.752.031Z" transform="translate(0 0)" />
+								<g transform="translate(10.156) rotate(90)">
+									<rect id="Rectangle_5" data-name="Rectangle 5" />
+									<path id="Path_2" data-name="Path 2" d="M10.392,1.855,8.616,0H0V8.348l1.965,1.808V6.021q0-1.509-.032-2.736l6.9,6.682L10.2,8.506,3.3,1.824q1.227.032,2.752.031Z" />
+								</g>
 							</svg>
 						</button>
 					</a>
